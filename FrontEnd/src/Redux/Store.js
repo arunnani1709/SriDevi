6407dd56f71846bbaf5d233a701577c6d6cad158
Siddesh N <siddeshsiddu049@gmail.com>
2025-07-02 00:17:39 +0530

    wipe all old files to update the new one

--- FrontEnd/src/Redux/Store.js
+++
@@ -1,9 +0,0 @@
-// src/Redux/Store.js
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../Components/Login/authSlice';
-
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
-});
